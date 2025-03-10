08558e38fe4b2b989e3b7497b507b475ed323427
soozynn <junsuejin98@gmail.com>
2025-03-10 15:29:19 +0900

    Time: 1 ms (47.47%), Space: 57.7 MB (18.52%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    let dummy = new ListNode(0); // 더미 노드
+    let current = dummy; // 병합된 리스트를 가리킬 포인터
+
+    while (list1 !== null && list2 !== null) {
+        if (list1.val < list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+
+    // 남은 노드를 이어 붙이기
+    if (list1 !== null) current.next = list1;
+    if (list2 !== null) current.next = list2;
+
+    return dummy.next; // 더미 노드의 next가 실제 병합된 리스트의 시작
+};
