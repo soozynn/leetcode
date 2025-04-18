e275b077a65ea13fe5aac497840019a8f5c7b4aa
soozynn <junsuejin98@gmail.com>
2025-04-18 21:32:21 +0900

    Time: 0 ms (100%), Space: 56.9 MB (88.76%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    let sum = 0;
+
+    const sumOfNode = (node, target) => {
+        if (!node) return false;
+
+        let copyTarget = target;
+        copyTarget += node.val;
+        // 절댓값 조건 수정
+        // if (copyTarget > targetSum) return false;
+        
+        const isLeafNode = !node.left && !node.right;
+
+        if (isLeafNode && copyTarget === targetSum) return true;
+        if (isLeafNode && copyTarget < targetSum) return false;
+
+        return sumOfNode(node.left, copyTarget) || sumOfNode(node.right, copyTarget);
+    };
+
+    return sumOfNode(root, sum);
+};
