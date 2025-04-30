4e27eb9339048fae1afe6547cf2abfa6903bef86
soozynn <junsuejin98@gmail.com>
2025-04-30 15:43:40 +0900

    Time: 0 ms (100%), Space: 53.6 MB (52.4%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+  if (num < 1) return false;
+
+  let left = 1;
+  let right = num;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const square = mid * mid;
+
+    if (square === num) {
+      return true;
+    }
+    
+    if (square < num) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return false;
+};
