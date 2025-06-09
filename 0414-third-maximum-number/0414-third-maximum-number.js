c9df81f7d1c6e69d09175800f9c68f7ae4a035bf
soozynn <junsuejin98@gmail.com>
2025-06-09 13:49:36 +0900

    Time: 4 ms (48.56%), Space: 55.9 MB (63.89%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    const map = new Set(nums);
+    const sortedNums = Array.from(map).sort((a, b) => b - a);
+
+    if (sortedNums.length < 3) {
+    return sortedNums[0];
+    }
+
+    return sortedNums[2];
+}
