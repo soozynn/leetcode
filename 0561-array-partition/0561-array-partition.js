48fd541efd69d44e6a2831412075ef8a927edb2b
soozynn <junsuejin98@gmail.com>
2026-01-17 12:34:26 +0900

    Time: 38 ms (78.57%), Space: 61 MB (31.19%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    let maximizedSumNumber = 0;
+
+    for (let i = 0; i < nums.length; i += 2) {
+        maximizedSumNumber += Math.min(nums[i], nums[i + 1]);
+    }
+
+    return maximizedSumNumber;
+};
